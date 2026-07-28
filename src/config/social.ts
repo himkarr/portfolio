211f78699a054771e694fce7b1322e8b736fd339
HimkarSingh <himkarsingh99@gmail.com>
2026-07-28 14:17:47 +0000

    feat: update LinkedIn and email links for accuracy

--- src/config/social.ts
+++ src/config/social.ts
@@ -1,13 +1,13 @@
 export const socialLinks = {
   github: "https://github.com/himkarr",
 
-  linkedin: "https://linkedin.com/in/himkarsingh99",
+  linkedin: "https://linkedin.com/in/himkar",
 
   twitter: "https://twitter.com/himkarr",
 
   tryhackme: "https://tryhackme.com/p/himkar",
 
-  email: "mailto:himkarsingh@proton.me",
+  email: "mailto:himkarrr@gmail.com",
 
   cal: "https://cal.com/himkar/",
 } as const;
